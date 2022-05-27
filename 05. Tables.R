41b60c86685b7dbdc61e0df154e43e908e23fe7d
Young Ri <yrlee@utexas.edu>
2022-05-27 13:45:18 -0500

    sim results

--- 05. Tables.R
+++ 05. Tables.R
@@ -9,34 +9,39 @@ library(kableExtra)
 # ANOVA
 rm(list = ls())
 load("sim_results.RData")
-results <- results %>% 
-  mutate(method = ifelse(method == "OLS", "OLS-CRVE",
-                         ifelse(method == "FE", "FE-CRVE", "CCREM")))
-results$method <- factor(results$method, 
-                         levels = c("CCREM", "OLS-CRVE", "FE-CRVE"))
-
-results_X <- results %>% 
-  mutate(beta = gamma100,
-         beta = as.factor(beta),
+results <- 
+  results %>% 
+  mutate(method = recode(method, 
+                         "OLS" = "OLS-CRVE",
+                         "FE" = "FE-CRVE"),
+         method = factor(method, levels = c("CCREM", "OLS-CRVE", "FE-CRVE")),
+         beta = as.factor(gamma100),
          H = as.factor(H),
-         J = as.factor(J),
-         ICC = as.factor(ICC),
-         rho = as.factor(rho),
+         J = factor(J, levels = c("30", "100")),
+         ICC_h = as.factor(ICC_h))
+
+results_assump <- 
+  results %>% 
+  filter(assumption %in% c("met", "heterosced", "exogeneity")) %>% 
+  mutate(assumption = recode(assumption,
+                             "met" = "Assumptions met",
+                             "heterosced" = "Homoscedasticity violated",
+                             "exogeneity" = "Exogeneity violated"),
          assumption = factor(assumption, 
                              levels = c("Assumptions met", 
                                         "Homoscedasticity violated", 
-                                        "Exogeneity violated"))) %>% 
-         # assumption = factor(assumption, levels = c("met", "heterosced", "exogeneity"))) %>% 
-  filter(cov == "X") 
+                                        "Exogeneity violated")))
+
+X <- results_assump %>% filter(cov == "X")
+W <- results_assump %>% filter(cov == "W")
 
 
 # ANOVA functions ---------------------------------------------------------------
-fml <- " ~ as.factor(method)+as.factor(beta)+as.factor(H)+as.factor(J)+as.factor(ICC)+as.factor(rho)+
-        as.factor(method)*(as.factor(beta)+as.factor(H)+as.factor(J)+as.factor(ICC)+as.factor(rho))+
-        as.factor(beta)*(as.factor(H)+as.factor(J)+as.factor(ICC)+as.factor(rho))+
-        as.factor(H)*(as.factor(J)+as.factor(ICC)+as.factor(rho))+
-        as.factor(J)*(as.factor(ICC)+as.factor(rho))+
-        as.factor(ICC)*as.factor(rho)"
+fml <- " ~ as.factor(method)+as.factor(beta)+as.factor(H)+as.factor(J)+as.factor(ICC_h)+
+        as.factor(method)*(as.factor(beta)+as.factor(H)+as.factor(J)+as.factor(ICC_h))+
+        as.factor(beta)*(as.factor(H)+as.factor(J)+as.factor(ICC_h))+
+        as.factor(H)*(as.factor(J)+as.factor(ICC_h))+
+        as.factor(J)*(as.factor(ICC_h))"
 
 tidy_func <- function(obj){
   anova_stats(obj) %>% as_tibble %>% 
@@ -47,41 +52,43 @@ tidy_func <- function(obj){
     mutate_if(is.numeric, round, 3)
 }
 
-table_anova <- function(outcome){
-  fit1 <- aov(as.formula(paste0(outcome, fml)), data = results_X %>% filter(assumption == "met"))
+table_anova <- function(outcome, cov){
+  fit1 <- aov(as.formula(paste0(outcome, fml)), data = cov %>% filter(assumption == "Assumptions met"))
   fit1 <- tidy_func(fit1)
   
-  fit2 <- aov(as.formula(paste0(outcome, fml)), data = results_X %>% filter(assumption == "heterosced"))
+  fit2 <- aov(as.formula(paste0(outcome, fml)), data = cov %>% filter(assumption == "Homoscedasticity violated"))
   fit2 <- tidy_func(fit2)
   
-  fit3 <- aov(as.formula(paste0(outcome, fml)), data = results_X %>% filter(assumption == "exogeneity"))
+  fit3 <- aov(as.formula(paste0(outcome, fml)), data = cov %>% filter(assumption == "Exogeneity violated"))
   fit3 <- tidy_func(fit3)
   
+  options(knitr.kable.NA = '')
   fit1 %>% left_join(fit2, by = "term") %>% 
-    left_join(fit3, by = "term") %>% 
+    left_join(fit3, by = "term") %>% filter(term != "Residuals") %>% 
     kable(digits = 4, col.names = c("", rep(c("partial_eta_sq", "size"), 3))) %>% 
     kable_styling(full_width = F) %>% 
-    add_header_above(c(" " = 1, "Assumptions met" = 2, "heterosced" = 2, "exogeneity" = 2))
+    add_header_above(c(" " = 1, "Assumptions met" = 2, "heterosced" = 2, "exogeneity" = 2)) %>% 
+    scroll_box(height = "255px")
 }
 
-# Table S4.1---------------------------------------------------------------
+# Table S5.1---------------------------------------------------------------
 results %>% filter(cov == "X" & assumption == "exogeneity") %>% 
   group_by(method, gamma100) %>% 
-  summarise(avg = mean(bias)) %>% 
+  summarise(avg = mean(bias), .groups = "drop") %>% 
   mutate(prop_avg = avg/gamma100) %>% 
   kable(digits = 7) %>%
   kable_styling(full_width = F) 
 
-# Table S4.2---------------------------------------------------------------
-table_anova("bias") 
+# Table S5.2---------------------------------------------------------------
+table_anova("bias", X) 
 
-# Table S4.3---------------------------------------------------------------
-table_anova("rmse")
+# Table S5.3---------------------------------------------------------------
+table_anova("rmse", X)
 
-# Table S4.4---------------------------------------------------------------
-table_anova("rej_rate")
+# Table S5.4---------------------------------------------------------------
+table_anova("rej_rate", X)
 
-# Table S4.5---------------------------------------------------------------
+# Table S5.5---------------------------------------------------------------
 results_assump <- results %>% 
   mutate(assumption = ifelse(assumption == "met", "Assumptions met",
                              ifelse(assumption == "heterosced", 
@@ -94,11 +101,11 @@ results_assump$assumption <- factor(results_assump$assumption,
 results_assump %>% 
   filter(cov == "X") %>% 
   group_by(assumption, method) %>% 
-  summarise(min = min(rej_rate),
-            avg = mean(rej_rate),
-            max = max(rej_rate)) %>% 
+  summarise(min = min(rej_rate, na.rm = TRUE),
+            avg = mean(rej_rate, na.rm = TRUE),
+            max = max(rej_rate, na.rm = TRUE), .groups = "drop") %>% 
   kable(digits = 3) %>%
   kable_styling(full_width = F) 
 
-# Table S4.6---------------------------------------------------------------
-table_anova("coverage")
+# Table S5.6---------------------------------------------------------------
+table_anova("coverage", X)
--- 06. Figures.R
+++ 06. Figures.R
@@ -4,38 +4,48 @@ library(wesanderson)
 
 rm(list = ls())
 load("sim_results.RData")
-results <- results %>% 
-  mutate(method = ifelse(method == "OLS", "OLS-CRVE",
-                  ifelse(method == "FE", "FE-CRVE", "CCREM")))
-results$method <- factor(results$method, 
-                         levels = c("CCREM", "OLS-CRVE", "FE-CRVE"))
-results_assump <- results %>% 
-  mutate(assumption = ifelse(assumption == "met", "Assumptions met",
-                             ifelse(assumption == "heterosced", 
-                                    "Homoscedasticity violated", 
-                                    "Exogeneity violated")))
-results_assump$assumption <- factor(results_assump$assumption, 
-                                    levels = c("Assumptions met", 
-                                               "Homoscedasticity violated", 
-                                               "Exogeneity violated"))
+results <- 
+  results %>% 
+  mutate(method = recode(method, 
+                         "OLS" = "OLS-CRVE",
+                         "FE" = "FE-CRVE"),
+         method = factor(method, levels = c("CCREM", "OLS-CRVE", "FE-CRVE")),
+         beta = as.factor(gamma100),
+         H = as.factor(H),
+         J = factor(J, levels = c("30", "100")),
+         ICC_h = as.factor(ICC_h))
+
+results_assump <- 
+  results %>% 
+  filter(assumption %in% c("met", "heterosced", "exogeneity")) %>% 
+  mutate(assumption = recode(assumption,
+                             "met" = "Assumptions met",
+                             "heterosced" = "Homoscedasticity violated",
+                             "exogeneity" = "Exogeneity violated"),
+         assumption = factor(assumption, 
+                             levels = c("Assumptions met", 
+                                        "Homoscedasticity violated", 
+                                        "Exogeneity violated")))
+
+X <- results_assump %>% filter(cov == "X")
+W <- results_assump %>% filter(cov == "W")
 
 # Figure 1----------------------------------------------------------------
-fig1_bias <- results %>%
-  mutate(beta = gamma100,
-         J = as.character(J)) %>%  
-  filter(cov == "X" & assumption == "exogeneity") %>% 
-  ggplot(aes(x = J, y = bias, fill = method, color = method)) + 
+fig1_bias <- X %>%
+  filter(assumption == "Exogeneity violated") %>% 
+  ggplot(aes(x = H, y = bias, fill = method, color = method)) + 
   geom_boxplot(alpha = .6, lwd = .1) + 
-  facet_grid( ~ paste0("IUCC = ", ICC_h), scales = "free_y") +  
-  labs(x = "Students per school", y = "Parameter Bias") + 
+  facet_grid(J_f ~ paste0("School IUCC = ", ICC_h), 
+             scales = "free_y") +  
+  labs(x = "Number of Schools", y = "Parameter Bias") + 
   theme_bw() +
   theme(text = element_text(size = 10),
         legend.title = element_blank(),
         legend.position = "bottom",
         plot.caption=element_text(hjust = 0),
         axis.text.x = element_text(angle=90, hjust=1)) +
-  scale_x_discrete(limits=c("30","100")) +
-  scale_y_continuous(limits = c(0, .01)) +
+  scale_x_discrete(limits=c("20","70","150")) +
+  scale_y_continuous(limits = c(0, .005)) +
   scale_fill_manual(values = c("#FF0000", "#00A08A", "#046C9A")) +
   scale_color_manual(values = c("#FF0000", "#00A08A", "#046C9A"))
 
@@ -44,10 +54,7 @@ fig1_bias
 dev.off()
 
 # Figure 2----------------------------------------------------------------
-fig2_rmse <- results_assump %>%
-  mutate(beta = gamma100,
-         H = as.character(H)) %>% 
-  filter(cov == "X") %>%
+fig2_rmse <- X %>%
   group_by(gamma100, gamma010, gamma002, H, G, ICC_h, J, assumption) %>% 
   mutate(rmse_std = 100*rmse/rmse[method == "CCREM"]) %>% 
   ggplot(aes(x = H, y = rmse_std, fill = method, color = method)) + 
@@ -70,10 +77,7 @@ fig2_rmse
 dev.off()
 
 # Figure 3----------------------------------------------------------------
-fig3_rej <- results_assump %>%
-  mutate(beta = gamma100,
-         H = as.character(H)) %>% 
-  filter(cov == "X") %>% 
+fig3_rej <- X %>%
   ggplot(aes(x = H, y = rej_rate, fill = method, color = method)) + 
   geom_hline(yintercept = .05, linetype = "dashed") +
   geom_boxplot(alpha = .6, lwd = .1) + 
@@ -95,10 +99,7 @@ fig3_rej
 dev.off()
 
 # Figure 4----------------------------------------------------------------
-fig4_cov <- results_assump %>%
-  mutate(beta = gamma100,
-         H = as.character(H)) %>% 
-  filter(cov == "X") %>% 
+fig4_cov <- X %>%
   ggplot(aes(x = H, y = coverage, fill = method, color = method)) + 
   geom_hline(yintercept = c(.925, .975), linetype = "dashed") +
   geom_hline(yintercept = c(.95)) +
@@ -121,14 +122,14 @@ fig4_cov
 dev.off()
 
 
-# Figure S4.1-------------------------------------------------------------
-s4_1_bias_met <- results %>%
+# Figure S5.1-------------------------------------------------------------
+s5_1_bias_met <- results_assump %>%
   mutate(beta = gamma100,
          H = as.character(H)) %>% 
-  filter(cov == "X" & assumption == "met") %>% 
+  filter(cov == "X" & assumption == "Assumptions met") %>% 
   ggplot(aes(x = H, y = bias, fill = method, color = method)) + 
   geom_boxplot(alpha = .6, lwd = .1) + 
-  facet_grid(paste("\u03C1 = ", rho) ~ paste0("IUCC = ", ICC_h), scales = "free_y") +  
+  facet_grid( ~ paste0("IUCC = ", ICC_h), scales = "free_y") +  
   labs(x = "Number of Schools", y = "Parameter Bias") + 
   theme_bw() +
   theme(text = element_text(size = 10),
@@ -140,18 +141,18 @@ s4_1_bias_met <- results %>%
   scale_fill_manual(values = c("#FF0000", "#00A08A", "#046C9A")) +
   scale_color_manual(values = c("#FF0000", "#00A08A", "#046C9A"))
 
-tiff("s4_1_bias_met.tiff", units="in", width=8, height=4.5, res=300)
-s4_1_bias_met
+tiff("s5_1_bias_met.tiff", units="in", width=8, height=4.5, res=300)
+s5_1_bias_met
 dev.off()
 
-# Figure S4.2-------------------------------------------------------------
-s4_2_bias_het <- results %>%
+# Figure S5.2-------------------------------------------------------------
+s5_2_bias_het <- results_assump %>%
   mutate(beta = gamma100,
          H = as.character(H)) %>% 
-  filter(cov == "X" & assumption == "heterosced") %>% 
+  filter(cov == "X" & assumption == "Homoscedasticity violated") %>% 
   ggplot(aes(x = H, y = bias, fill = method, color = method)) + 
   geom_boxplot(alpha = .6, lwd = .1) + 
-  facet_grid(paste("\u03C1 = ", rho) ~ paste0("IUCC = ", ICC_h), scales = "free_y") +  
+  facet_grid( ~ paste0("IUCC = ", ICC_h), scales = "free_y") +  
   labs(x = "Number of Schools", y = "Parameter Bias") + 
   theme_bw() +
   theme(text = element_text(size = 10),
@@ -163,14 +164,14 @@ s4_2_bias_het <- results %>%
   scale_fill_manual(values = c("#FF0000", "#00A08A", "#046C9A")) +
   scale_color_manual(values = c("#FF0000", "#00A08A", "#046C9A"))
 
-tiff("s4_2_bias_het.tiff", units="in", width=8, height=4.5, res=300)
-s4_2_bias_het
+tiff("s5_2_bias_het.tiff", units="in", width=8, height=4.5, res=300)
+s5_2_bias_het
 dev.off()
 
 
 
-# Figure S4.3-------------------------------------------------------------
-s4_3_rmse_met <- results_assump %>%
+# Figure S5.3-------------------------------------------------------------
+s5_3_rmse_met <- results_assump %>%
   mutate(beta = gamma100,
          H = as.character(H)) %>% 
   filter(cov == "X") %>%
@@ -188,12 +189,12 @@ s4_3_rmse_met <- results_assump %>%
   scale_fill_manual(values = c("#FF0000", "#00A08A", "#046C9A")) +
   scale_color_manual(values = c("#FF0000", "#00A08A", "#046C9A"))
 
-tiff("s4_3_rmse_met.tiff", units="in", width=8, height=7, res=300)
-s4_3_rmse_met
+tiff("s5_3_rmse_met.tiff", units="in", width=8, height=7, res=300)
+s5_3_rmse_met
 dev.off()
 
-# Figure S4.4-------------------------------------------------------------
-s4_4_rmse <- results_assump %>%
+# Figure S5.4-------------------------------------------------------------
+s5_4_rmse <- results_assump %>%
   mutate(beta = gamma100,
          H = as.character(H)) %>% 
   filter(cov == "X" & assumption == "Exogeneity violated") %>% 
@@ -201,7 +202,7 @@ s4_4_rmse <- results_assump %>%
   mutate(rmse_std = 100*rmse/rmse[method == "CCREM"]) %>% 
   ggplot(aes(x = H, y = rmse_std, fill = method, color = method)) + 
   geom_boxplot(alpha = .6, lwd = .1) + 
-  facet_grid(paste("\u03C1 = ", rho) ~ paste0("IUCC = ", ICC_h), scales = "free_y") +
+  facet_grid( ~ paste0("IUCC = ", ICC_h), scales = "free_y") +
   labs(x = "Number of Schools", y = "Root Mean Squared Error") + 
   theme_bw() +
   theme(text = element_text(size = 10),
@@ -213,41 +214,37 @@ s4_4_rmse <- results_assump %>%
   scale_fill_manual(values = c("#FF0000", "#00A08A", "#046C9A")) +
   scale_color_manual(values = c("#FF0000", "#00A08A", "#046C9A"))
 
-tiff("s4_4_rmse_exo.tiff", units="in", width=8, height=4.5, res=300)
-s4_4_rmse
+tiff("s5_4_rmse_exo.tiff", units="in", width=8, height=4.5, res=300)
+s5_4_rmse
 dev.off()
 
 
 
 
 
-# Figure S4.5 ------------------------------------------------------------
-s4_5_convg <- results_assump %>%
+# Figure S5.5 ------------------------------------------------------------
+s5_5_convg <- results_assump %>%
   mutate(beta = gamma100,
          H = as.character(H)) %>% 
-  filter(cov == "X" & method == "CCREM") %>% 
-  group_by(assumption, method, ICC_h, H, J, rho) %>% 
-  summarise(non_na_count = sum(!is.na(est)),
-            all_count = n()) %>% 
-  mutate(pct = non_na_count/all_count) %>% 
-  ggplot(aes(x = H, y = pct, fill = method, color = method)) + 
+  filter(method == "CCREM") %>% 
+  ggplot(aes(x = H, y = convergence_rate, fill = method, color = method)) + 
   geom_bar(stat="identity", position=position_dodge(), 
            width=0.75, alpha = .6) +
-  facet_grid(assumption + paste("\u03C1 = ", rho) ~ paste0("IUCC = ", ICC_h) + paste("J = ", J), 
+  facet_grid(assumption ~ paste0("IUCC = ", ICC_h) + paste("J = ", J), 
              scales = "free_y") +
   labs(x = "Number of Schools", y = "Rate of Convergence") + 
   theme_bw() +
   theme(text = element_text(size = 10),
         legend.title = element_blank(),
-        legend.position = "bottom",
+        legend.position = "none",
         plot.caption=element_text(hjust = 0),
         axis.text.x = element_text(angle=90, hjust=1)) +
   scale_x_discrete(limits=c("20","70","150")) +
-  scale_fill_manual(values = c("#FF0000", "#00A08A", "#046C9A")) +
-  scale_color_manual(values = c("#FF0000", "#00A08A", "#046C9A"))
+  scale_fill_manual(values = c("grey", "#00A08A", "#046C9A")) +
+  scale_color_manual(values = c("grey", "#00A08A", "#046C9A"))
 
-tiff("s4_5_convg.tiff", units="in", width=8, height=10, res=300)
-s4_5_convg
+tiff("s5_5_convg.tiff", units="in", width=8, height=10, res=300)
+s5_5_convg
 dev.off()
 
 
